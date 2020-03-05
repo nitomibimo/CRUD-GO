da9b45243c490a31eb2f8a0df6b87cab0c2e17b8
nitomi <nitomi.bimo@gmail.com>
2020-03-05 10:48:50 +0700

    add comment connect db

--- api/controller/base.go
+++ api/controller/base.go
@@ -56,16 +56,8 @@ func (s *Server) Initialize(Dbdriver, DbUser, DbPassword, DbPort, DbHost, DbName
 			fmt.Printf("We are connected to the %s database", Dbdriver)
 		}
 	}
-	if Dbdriver == "postgres" {
-		DBURL := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", DbHost, DbPort, DbUser, DbName, DbPassword)
-		s.DB, err = gorm.Open(Dbdriver, DBURL)
-		if err != nil {
-			fmt.Printf("Cannot connect to %s database", Dbdriver)
-			log.Fatal("This is the error:", err)
-		} else {
-			fmt.Printf("We are connected to the %s database", Dbdriver)
-		}
-	}
+
+	// you can add if condition in here if you have other db connection and add in your .env
 
 	s.DB.Debug().AutoMigrate(&models.User{}, &models.Post{}) //database migration
 
